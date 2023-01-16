a291d538ce2aafd75aa06ed8497c30386a2cce55
Candi Schoen <cschoen@cschoenmbpro.local.vpn.lodgenet.com>
2023-01-16 10:36:14 -0600

    Added auth sub app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+/* import MarketingApp from './components/MarketingApp';
+import AuthApp from './components/AuthApp'; */
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import Progress from './components/Progress';
+import Header from './components/Header';
+
+//This will only load the component when we show it on the screen
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
@@ -10,12 +17,20 @@ const generateClassName = createGenerateClassName({
 
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                <Header />
-                <MarketingApp />
+                <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn}/>
+                <Suspense fallback={<Progress />}>
+                <Switch>
+                    <Route path="/auth">
+                        <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+                    </Route>
+                    <Route path="/" component={MarketingLazy} />
+                </Switch>
+                </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -1,9 +1,9 @@
-import { mount } from 'marketing/MarketingApp';
+import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 //comments
-export default () => {
+export default ( { onSignIn }) => {
     const ref = useRef(null);
     const history = useHistory();
 
@@ -22,6 +22,10 @@ export default () => {
                     history.push(nextPathname);
                     // console.log(nextPathname)
                 }
+            },
+            //Could also just call onSignIn without the function since its the same name
+            onSignIn: () => {
+                onSignIn();
             }
         });
 
